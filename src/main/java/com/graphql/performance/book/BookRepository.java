2999d1a36bdc486c4885e8796b23f2036351127d
Amlan Deep Nandi <amlandeep.nandi@gmail.com>
2025-03-22 22:52:16 +0530

    Spring Boot version update

--- src/main/java/com/graphql/performance/book/BookRepository.java
+++ src/main/java/com/graphql/performance/book/BookRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long>
 {
-
 	List<Book> findAllByTitleContainsIgnoreCase(String title);
 
 	Arrays findAllByAuthorIdIn(List<Long> authorIds);
